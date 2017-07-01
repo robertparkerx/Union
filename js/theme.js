4763becfce8518b3a5d2607d9ba66681799dc81a
exactprecisions <exactprecisions@gmail.com>
2017-07-01 09:52:45 -0500

    style templates

---
+++ js/theme.js
@@ -0,0 +1,11 @@
+/**
+ * Created by exact on 7/1/2017.
+ */
+(function($){
+    $(function(){
+
+        $('.button-collapse').sideNav();
+        $('.parallax').parallax();
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
