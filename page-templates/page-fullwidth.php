8b8acc368b277cf42c03e63fb521ce03d540099e
exactprecisions <exactprecisions@gmail.com>
2017-07-01 22:28:47 -0500

    + page template + style

---
+++ page-templates/page-fullwidth.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: exact
+ * Date: 5/7/2017
+ * Time: 10:48 AM
+ * Template Name: Page - Full width
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="medium-12 large-12 cell content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php
+			while ( have_posts() ) : the_post();
+
+				get_template_part( 'template-parts/content', 'page' );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			endwhile; // End of the loop.
+			?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
